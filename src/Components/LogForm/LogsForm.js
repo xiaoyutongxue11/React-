a9ed1c89f16997990a8fe9c2921fc593341ce9b9
tyy123 <1572235056@qq.com>
2024-01-27 22:50:17 +0800

    state双向绑定

--- src/Components/LogForm/LogsForm.js
+++ src/Components/LogForm/LogsForm.js
@@ -1,18 +1,19 @@
 import React from "react";
 import Card from "../UI/Card/Card";
 import "./LogsForm.css";
+import { useState } from "react"; 
 const LogsForm = () => {
-  let inputDate = "";
-  let inputDesc = "";
-  let inputTime = 0;
+  const [inputDate, setInputDate] = useState("");
+  const [inputDesc, setInputDesc] = useState("");
+  const [inputTime, setInputTime] = useState("");
   const dateChange = (e) => {
-    inputDate = e.target.value;
+    setInputDate(e.target.value);
   };
   const descChange = (e) => {
-    inputDesc = e.target.value;
+    setInputDesc(e.target.value);
   };
   const timeChange = (e) => {
-    inputTime = e.target.value;
+    setInputTime(e.target.value);
   };
   const formSubmit = (e) => {
     e.preventDefault();
@@ -22,21 +23,39 @@ const LogsForm = () => {
       time: +inputTime,
     };
     console.log(logData);
+    setInputDate("");
+    setInputDesc("");
+    setInputTime("");
   };
   return (
     <Card className="logs-form">
-      <form onSubmit={formSyubmit}>
+      <form onSubmit={formSubmit}>
         <div className="logs-item">
           <label htmlFor="date">日期</label>
-          <input id="date" type="date" onChange={dateChange} />
+          <input
+            id="date"
+            type="date"
+            onChange={dateChange}
+            value={inputDate}
+          />
         </div>
         <div className="logs-item">
           <label htmlFor="desc">内容</label>
-          <input id="desc" type="text" onChange={descChange} />
+          <input
+            id="desc"
+            type="text"
+            onChange={descChange}
+            value={inputDesc}
+          />
         </div>
         <div className="logs-item">
           <label htmlFor="time">时长</label>
-          <input id="time" type="number" onChange={timeChange} />
+          <input
+            id="time"
+            type="number"
+            onChange={timeChange}
+            value={inputTime}
+          />
         </div>
         <div className="logs-btn">
           <button>添加</button>
