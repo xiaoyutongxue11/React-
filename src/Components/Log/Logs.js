b3e23e3d526804790e9b65be130b564b193e65ab
tyy123 <1572235056@qq.com>
2024-01-29 18:00:52 +0800

    列表为空时显示处理

--- src/Components/Log/Logs.js
+++ src/Components/Log/Logs.js
@@ -3,15 +3,17 @@ import LogItem from "./LogItem";
 import "./Logs.css";
 import Card from "../UI/Card/Card";
 const Logs = (props) => {
+  const logsData = props.logsData.map((item) => (
+    <LogItem
+      data={item}
+      key={item.id}
+      onDeleteLog={() => props.onDeleteLog(item.id)}
+    />
+  ));
+  const nullData = <p className="nullData">没有找到学习记录</p>;
   return (
     <Card className="logs">
-      {props.logsData.map((item) => (
-        <LogItem
-          data={item}
-          key={item.id}
-          onDeleteLog={() => props.onDeleteLog(item.id)}
-        />
-      ))}
+      {props.logsData.length === 0 ? nullData : logsData}
     </Card>
   );
 };
